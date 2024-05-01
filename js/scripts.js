0a60f29d9b35cba392f1950440ed6e372b890d9e
lynn-miao <87213430+lynn-miao@users.noreply.github.com>
2024-04-30 20:09:50 -0400

    line colors

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmY
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v12',
-    center: [-73.99300, 40.70184],
-    zoom: 14
+    center: [-73.92081, 40.72278],
+    zoom: 10
 });
 
 map.addControl(new mapboxgl.NavigationControl());
@@ -15,20 +15,69 @@ map.on('load', () => {
         map.getStyle().layers
     )
 
-    // add the pluto data
+    // add the subway data
 
     map.addSource('subway', {
         "type": "geojson",
         "data": 'data/subway.geojson'
     })
 
+    // color the subway lines by color
     map.addLayer({
         'id': 'subway-line',
         'type': 'line',
         'source': 'subway',
         'layout': {},
         'paint': {
-            'line-color': '#000',
+            'line-color': [
+                'match',
+                ['get', 'rt_symbol'],
+                'A',
+                '#0039A6',
+                'C',
+                '#0039A6',
+                'E',
+                '#0039A6',
+                'B',
+                '#FF6319',
+                'D',
+                '#FF6319',
+                'F',
+                '#FF6319',
+                'M',
+                '#FF6319',
+                'G',
+                '#6CBE45',
+                'J',
+                '#996633',
+                'Z',
+                '#996633',
+                'L',
+                '#A7A9AC',
+                'R',
+                '#FCCC0A',
+                'W',
+                '#FCCC0A',
+                'N',
+                '#FCCC0A',
+                'Q',
+                '#FCCC0A',
+                '1',
+                '#EE352E',
+                '2',
+                '#EE352E',
+                '3',
+                '#EE352E',
+                '4',
+                '#00933C',
+                '5',
+                '#00933C',
+                '6',
+                '#00933C',
+                '7',
+                '#B933AD',
+                '#ccc'
+            ],
             'line-width': 2,
         }
     });
